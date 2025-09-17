38ba6b40fb3a7a9f66bb594eb7a78b353ef3d653
Jose Sosa <jjose.sosa9@gmail.com>
2025-09-17 15:18:32 -0400

    fixing redirect ui

--- frontend/app/components/SpotifyLogin.tsx
+++ frontend/app/components/SpotifyLogin.tsx
@@ -1,15 +1,15 @@
 import { env } from "process";
 export const spotifyLogin = () => {
 
-    // Your app's credentials from the Spotify Dashboard
+
+    // 
+
     const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
-    // The page in your app that Spotify should send the user back to
-    const redirectUri = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID + "/api/auth/callback";
-    // The permissions your app is requesting
+    const redirectUri = `${window.location.origin}/api/auth/callback`;
     const scopes = "user-read-private user-read-email playlist-read-private";
 
-    // Construct the URL and redirect
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
-    window.location.href = authUrl; // This is the crucial redirect
+
+    window.location.href = authUrl;
 
 };
