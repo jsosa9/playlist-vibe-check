b63be4ec32cc1f6d57dc4e051c26aef0d910b792
Jose Sosa <jjose.sosa9@gmail.com>
2025-09-17 15:41:45 -0400

    more link changes

--- frontend/app/components/SpotifyLogin.tsx
+++ frontend/app/components/SpotifyLogin.tsx
@@ -3,7 +3,10 @@ export const spotifyLogin = () => {
 
 
     const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
-    const redirectUri = `${window.location.origin}/api/auth/callback`;
+    const redirectUri =
+        process.env.NODE_ENV === "production"
+            ? "https://playlist-vibe-check.vercel.app/api/auth/callback"
+            : "http://localhost:3000/api/auth/callback";
     const scopes = "user-read-private user-read-email playlist-read-private";
     console.log(redirectUri);
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
