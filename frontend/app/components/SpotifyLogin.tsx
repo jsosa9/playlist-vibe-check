664c365587594dd9475623aec8e14ef5aafd50ce
Jose Sosa <jjose.sosa9@gmail.com>
2025-09-17 15:36:41 -0400

    debugging and env changes

--- frontend/app/components/SpotifyLogin.tsx
+++ frontend/app/components/SpotifyLogin.tsx
@@ -2,12 +2,10 @@ import { env } from "process";
 export const spotifyLogin = () => {
 
 
-    // 
-
     const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
     const redirectUri = `${window.location.origin}/api/auth/callback`;
     const scopes = "user-read-private user-read-email playlist-read-private";
-
+    console.log(redirectUri);
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
 
     window.location.href = authUrl;
